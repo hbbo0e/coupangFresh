27e5239a8a80e6a83ba184bedfaa6fc8085e00fb
hbbo0e <44goodluck44@gmail.com>
2024-04-16 15:43:07 +0900

    4 여러 페이지 만들기 Routing

---
+++ app/list/page.js
@@ -0,0 +1,14 @@
+export default function List(){
+
+    return (
+      <div>
+        <h4 className="title"> 신선 식품 판매목록 </h4>
+        <div className="food">
+            <h5> 상품 1 3000원 </h5>
+        </div>
+        <div className="food">
+            <h5> 상품 2 2000원 </h5>
+        </div>
+      </div>
+    )
+  }
