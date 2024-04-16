622f1e04248b152510681d3893ad983a6efc2a94
hbbo0e <44goodluck44@gmail.com>
2024-04-16 16:47:44 +0900

    map() 활용

--- app/list/page.js
+++ app/list/page.js
@@ -1,14 +1,19 @@
 export default function List(){
 
+    let products = [ '토마토', '파스타', '코코넛' ]
+
     return (
       <div>
         <h4 className="title"> 신선 식품 판매목록 </h4>
-        <div className="food">
-            <h5> 상품 1 3000원 </h5>
-        </div>
-        <div className="food">
-            <h5> 상품 2 2000원 </h5>
-        </div>
+        {
+            products.map(()=>{
+                return (
+                    <div className="food">
+                        <h5> {products[0]} 1000원 </h5>
+                    </div>
+                )
+            })
+        }
       </div>
     )
   }
